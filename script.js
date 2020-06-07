15c12e310743f289c9ce34cea5edc86912ca5a6f
Cerulean-Ash <sesshiru@gmail.com>
2020-06-07 12:37:23 +0100

    implemented scroll progress indicator bar with JS

--- script.js
+++ script.js
@@ -1,3 +1,22 @@
 const toggleNavbar = () => {
   document.getElementById("navbar").classList.toggle("active");
 };
+
+// When the user scrolls the page, execute myFunction
+
+window.onscroll = () => {
+  scrollFunction();
+};
+
+function scrollFunction() {
+  // position of scroll bar in pixels
+  let winScroll = document.documentElement.scrollTop; // document.body.scrollTop; ||
+  //get height excluding first page
+  let height =
+    document.documentElement.scrollHeight - // total page height
+    document.documentElement.clientHeight; // how tall the client window is
+  let scrolled = (winScroll / height) * 100;
+  document.getElementById("myBar").style.height = scrolled + "%";
+}
+
+// adapted from https://www.w3schools.com/howto/howto_js_scroll_indicator.asp
